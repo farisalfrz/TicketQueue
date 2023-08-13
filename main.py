1ee2ed0071c814ab438228cb9d79b58224bbe6cc
RashadRZ <rashadzulkarnaen@gmail.com>
2023-08-13 22:12:00 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from gtts import gTTS
+from playsound import playsound
+
+# sounds
+# tts = gTTS('to table one')
+# tts.save('sounds/to_table_one.mp3')
+
+# functions
+
+# heap
+
+# run
+# do
+print('=' * 50)
+print(f"{'TABLE 1': <20}{'TABLE 2': <20}{'NEXT': <20}")
+print('=' * 50)
+print(f"{'B001': <20}{'P001': <20}{'B002': <20}")
+print('-' * 50)
+
+print('1. Add Queue Business')
+print('2. Add Queue Personal')
+print('3. Table 1 Calling')
+print('4. Table 2 Calling')
+print('5. Exit')
+
+# input
+
+# if else from input
+
+# while input != exit
