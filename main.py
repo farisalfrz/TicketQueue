54259ab09b06600e5e26abb72027853055a85c0e
farisalfrz <faris.alfarizi109@gmail.com>
2023-08-14 10:32:07 +0700

    adding function table1_calling, table2_calling

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import heapq
+
 from playsound import playsound
 
 
@@ -6,19 +7,48 @@ from playsound import playsound
 def add_business():
     # hubla
     pass
+
+
 def table1_calling():
-    pass
+    global table1
+
+    called = heapq.heappop(queue)
+    playsound('sounds/queue_number.mp3')
+    playsound(f'sounds/{called[0]}.mp3')
+    playsound(f'sounds/{called[1]}.mp3')
+    playsound(f'sounds/{called[2]}.mp3')
+    playsound(f'sounds/{called[3]}.mp3')
+    playsound('sounds/to_table_two.mp3')
+    table1 = called
+
+
+def table2_calling():
+    global table2
+
+    called = heapq.heappop(queue)
+    playsound('sounds/queue_number.mp3')
+    playsound(f'sounds/{called[0]}.mp3')
+    playsound(f'sounds/{called[1]}.mp3')
+    playsound(f'sounds/{called[2]}.mp3')
+    playsound(f'sounds/{called[3]}.mp3')
+    playsound('sounds/to_table_two.mp3')
+    table2 = called
 
 
 # heap
-queue = []
+queue = ["B001", "B002", "P001"]
+table1 = ""
+table2 = ""
 
 # run
 # do
+table1_calling()
+table2_calling()
+
 print('=' * 50)
 print(f"{'TABLE 1': <20}{'TABLE 2': <20}{'NEXT': <20}")
 print('=' * 50)
-print(f"{'B001': <20}{'P001': <20}{'B002': <20}")
+print(f"{table1: <20}{table2: <20}{queue[0]: <20}")
 print('-' * 50)
 
 print('1. Add Queue Business')
