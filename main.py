8324472135cd9910f5ad7e3be3d127be4ab604d7
RashadRZ <rashadzulkarnaen@gmail.com>
2023-08-14 09:39:10 +0700

    add queue

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ tts.save('sounds/9.mp3')
 
 # heap
 queue = []
-queue.append(1)
-queue.append(2)
-queue.append(3)
+queue.append(4)
+queue.append(5)
+queue.append(6)
+queue.append(7)
 
 # run
 # do
